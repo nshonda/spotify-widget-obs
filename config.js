311cfabdf211c613c4a6c6a5c3f715aad7d4e214
Natali Honda <natalihonda@gmail.com>
2024-01-01 22:45:59 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,65 @@
+// config.js
+// Last modified: Tuesday, 12th September 2023, 12:40:39 pm
+
+// Spotify Credentials - DO NOT SHARE TO ANYONE
+
+export const client_id = "4203e4f4d1924ba4b2ca89ef001cb276";
+export const client_secret = "e15953794d78409b95f8d8cabab8d7c5";
+export const refresh_token = "AQBwXlfF1CCR58Gjrxf6pSIRDLNwNPhKdLJNFM_8JWbmBTFilto7OLHLM2wRWQc4z6uaeItF1VlJME5yTsQWWM_-S8BmNhj3MD-ljzmiUD13Z56E6yNhmZrV9bF-VoFoz8s";
+
+
+// -- Widget config
+
+export const refresh_rate = 1; // Data refresh rate in seconds. Do not go below 0.5s.
+export const hide_cover = false; // show or hide album cover entirely. [true/false]
+export const album_cover_bar = true; // show or hide music bar animation. [true/false]
+export const album_cover_pulse = true; // Make album cover pulse. [true/false]
+export const album_cover_circle = true; // Make album circle [true/false]
+export const album_cover_spin = true; // Make album cover spin. [true/false]
+export const music_player_background_style = 6; // Widget themes. Choose between [1 ~ 6].
+
+// --- Themes data
+
+export const widget_style_map = {
+  1: {
+    background: 'linear-gradient(90deg, #f7c0ec 0%, #a9bfea 100%)',
+    color: '#403f3f',
+    pulse_color: '#db79cbc7',
+  },
+  2: {
+    background: 'linear-gradient(90deg, #295270 0%, #534176 100%)',
+    color: '#dfdfdf',
+    pulse_color: '#f395e8b8',
+  },
+  3: {
+    background: 'linear-gradient(90deg, #caf2ef 0%, #c8efdb 50%, #f2baf1 100%)',
+    color: '#403f3f',
+    pulse_color: '#f2baf1ba',
+  },
+  4: {
+    background: 'linear-gradient(120deg, #1a1700, #1c1901, #212004, #282b08, #2f390c, #364a0f, #395c15, #396f1e, #338028, #298f32, #1a983a, #119c3d)',
+    color: '#FFFFFF',
+    pulse_color: '#66f76bd9',
+  },
+  5: {
+    background: 'linear-gradient(270deg, #0ab5ae 0%, #45386a 100%)',
+    color: '#FFFFFF',
+    pulse_color: '#50ddafd1',
+  },
+  6: {
+    background: 'radial-gradient( circle 369px at -2.9% 12.9%,  rgba(247,234,163,1) 0%, rgba(236,180,238,0.56) 46.4%, rgba(163,203,247,1) 100.7% )',
+    color: '#000000',
+    pulse_color: '#cda4e7d1',
+  }
+};
+
+//!SECTION ████████████████████ Config End
+
+/* ████████████████████████████████████████ 
+
+Any trouble, questions, bugs, custom commission please contact me @
+
+✨ discord.gg/e5DQ8yKnj6
+✨ twitter.com/jhooo_o
+
+*/
